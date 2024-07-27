5ac39a21484f12ae91dcef3fc33f475bb44e033e
Brianlucca <brianlucca2002@gmail.com>
2024-07-27 16:58:28 -0300

    feat: added registration and login page

--- src/pages/signIn/index.jsx
+++ src/pages/signIn/index.jsx
@@ -1,24 +1,25 @@
 import { useFormik } from "formik";
 import React from "react";
-import { useNavigate } from "react-router";
-import signUpValidationSchema from "../../../validation/signUpValidation";
+import signInValidationSchema from "../../../validation/signInValidation"; 
 import ImgForm from "../../assets/image/image-form.jpeg";
 import Footer from "../../components/footer";
 import Header from "../../components/header";
+import { useNavigate } from "react-router";
 
-function SignUp() {
-  const navigate = useNavigate();
+function SignIn() {
+  const navigate = useNavigate()
 
   const formik = useFormik({
     initialValues: {
-      fullname: "",
       email: "",
       password: "",
     },
-    validationSchema: signUpValidationSchema,
+    validationSchema: signInValidationSchema,
     onSubmit: () => {
-      localStorage.setItem("login", true);
-      navigate("/signin");
+      const isLoggedin = localStorage.getItem("login") === "true";
+      if(isLoggedin === true) {
+        navigate('/')
+      }
     },
   });
 
@@ -35,31 +36,9 @@ function SignUp() {
         </div>
         <div className="w-full lg:w-1/2 flex items-center justify-center p-8">
           <div className="w-full max-w-md">
-            <h2 className="font-medium text-4xl mb-4">Crie sua conta</h2>
+            <h2 className="font-medium text-4xl mb-4">Realize o login</h2>
             <p className="mb-6">Digite seus dados abaixo</p>
             <form onSubmit={formik.handleSubmit} className="space-y-4">
-              <div>
-                <label
-                  htmlFor="fullname"
-                  className="block text-sm font-medium text-gray-700"
-                >
-                  Nome
-                </label>
-                <input
-                  type="text"
-                  id="fullname"
-                  name="fullname"
-                  onChange={formik.handleChange}
-                  onBlur={formik.handleBlur}
-                  value={formik.values.fullname}
-                  className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:border-gray-500 sm:text-sm"
-                />
-                {formik.touched.fullname && formik.errors.fullname ? (
-                  <div className="text-red-600 text-sm">
-                    {formik.errors.fullname}
-                  </div>
-                ) : null}
-              </div>
               <div>
                 <label
                   htmlFor="email"
@@ -107,19 +86,18 @@ function SignUp() {
               <div className="flex flex-col space-y-4">
                 <button
                   type="submit"
-                  disabled={formik.isValid === false}
                   className="bg-gray-600 rounded px-4 py-2 text-white hover:bg-gray-700"
                 >
-                  Criar a conta
+                  Entre
                 </button>
                 <button
                   type="button"
                   onClick={() => {
-                    navigate("/signin");
+                    navigate('/signup')
                   }}
                   className="bg-white rounded px-4 py-2 border-2 border-gray-400 text-black hover:bg-gray-100"
                 >
-                  Já tem conta? Realize o login
+                  Não tem conta? Realize o cadastro
                 </button>
               </div>
             </form>
@@ -131,4 +109,4 @@ function SignUp() {
   );
 }
 
-export default SignUp;
+export default SignIn;
