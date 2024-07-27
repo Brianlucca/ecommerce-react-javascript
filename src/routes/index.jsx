6b0abb514a008d6419fd41c787a03fb342cc677f
Brianlucca <brianlucca2002@gmail.com>
2024-07-27 16:56:34 -0300

    feat: added route sigin

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -7,6 +7,7 @@ import HomePage from "../pages/homePage";
 import ProductDetails from "../pages/productDetails";
 import SearchResults from "../pages/searchResults";
 import SignUp from "../pages/signUp";
+import SignIn from "../pages/signIn";
 
 function RenderRoutes() {
   return (
@@ -19,6 +20,7 @@ function RenderRoutes() {
         <Route path="/favorites" element={<FavoriteProducts />} />
         <Route path="/search" element={<SearchResults />} />
         <Route path="/signup" element={<SignUp />} />
+        <Route path="/signin" element={<SignIn />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
